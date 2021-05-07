5e1e2abe9b7be0e7fe7664a8e188a8fab98a5cf4
QHfirewater <57171281+QHfirewater@users.noreply.github.com>
2021-05-07 22:43:17 +0800

    first add files
    file is very important

---
+++ draw.py
@@ -0,0 +1,19 @@
+# -*- encoding: utf-8 -*-
+'''
+@File    :   draw.py
+@Time    :   2020/02/26 10:59:58
+@Author  :   Sandwich 
+@Version :   1.0
+@Contact :   122079260@qq.com
+'''
+
+
+from turtle import *
+
+if __name__ == "__main__":
+    ht()
+    speed(10)
+    for i in range(500):
+        forward(i)
+        left(91)
+    done()
